5df71e0d3b0a2f08af0cd2e8def7084c1037d97e
Mauro Giménez <maurogimenezb@gmail.com>
2021-04-13 23:12:57 -0400

    Clase del 20210413 - [LISTAS] Add y delete.

---
+++ AddDeleteList/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace AddDeleteList
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.text_nombres = new System.Windows.Forms.TextBox();
+            this.list_nombres = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text_nombres
+            // 
+            this.text_nombres.Location = new System.Drawing.Point(70, 31);
+            this.text_nombres.Name = "text_nombres";
+            this.text_nombres.Size = new System.Drawing.Size(100, 23);
+            this.text_nombres.TabIndex = 2;
+            // 
+            // list_nombres
+            // 
+            this.list_nombres.FormattingEnabled = true;
+            this.list_nombres.ItemHeight = 15;
+            this.list_nombres.Location = new System.Drawing.Point(70, 107);
+            this.list_nombres.Name = "list_nombres";
+            this.list_nombres.Size = new System.Drawing.Size(120, 94);
+            this.list_nombres.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.list_nombres);
+            this.Controls.Add(this.text_nombres);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text_nombres;
+        private System.Windows.Forms.ListBox list_nombres;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AddDeleteList/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddDeleteList
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> listanombres = new List<string>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre;
+
+            nombre = text_nombres.Text;
+            listanombres.Add(nombre);
+
+            list_nombres.DataSource = null;
+            list_nombres.DataSource = listanombres;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listanombres.Remove(text_nombres.Text);
+
+            list_nombres.DataSource = null;
+            list_nombres.DataSource = listanombres;
+        }
+    }
+}
